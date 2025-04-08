646b8827d4bb9a2b9eae3818f13bc498ff7568d6
AnjaliKachhi <anjalikachhi@gmail.com>
2025-04-08 13:26:07 +0530

    first commit

--- democlass/src/Add.java
+++ democlass/src/Add.java
@@ -3,6 +3,8 @@ public class Add {
     public static void main(String[] args) throws Exception {
         int a=26;
         int b=34; 
+        System.out.println(a);
+        System.out.println(b);
         System.out.println(a+b);
     }
 }
