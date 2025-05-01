013ba304cde8e81c554b4181e60351ac656cabf5
AnjaliKachhi <anjalikachhi@gmail.com>
2025-05-01 07:01:38 +0530

    loop

--- democlass/src/function.java
+++
@@ -1,8 +0,0 @@
-public class function {
-    static void sum(){
-        System.out.println("hello sweety");
-    }
-
-public static void main(String[] args){
-   sum();
-        }}
--- democlass/src/sum.java
+++
@@ -1,9 +0,0 @@
-public class sum {
-    static void add(){
-        int a=3,b=4;
-        System.out.println(a+b);
-    }
-    public static void main(String[] args) {
-         add();
-    }
-}
