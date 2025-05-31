19093c9c96462a7a4286adc273c6dc158eacdf6f
AnjaliKachhi <anjalikachhi69972@gmail.com>
2025-05-31 13:22:04 +0530

    interface

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+interface Vehicle {
+    void start();  
+}
+
+
+class Car implements Vehicle {
+    public void start() {
+        System.out.println("Car is starting...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle myCar = new Car();  
+        myCar.start();  
+    }
+}
